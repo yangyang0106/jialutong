891e7f96c7ede4d920eed903382e434a6b54b1bb
孙向阳 <sunxiangyang@ruigushop.com>
2026-06-26 13:51:40 +0800

    Use fixed production upload config

--- config/upload.js
+++ config/upload.js
@@ -1,24 +1,6 @@
-let localConfig = {};
-
-function shouldUseLocalConfig() {
-  try {
-    return typeof __wxConfig !== "undefined" && __wxConfig.envVersion === "develop";
-  } catch (error) {
-    return false;
-  }
-}
-
-if (shouldUseLocalConfig()) {
-  try {
-    localConfig = require("./upload.local");
-  } catch (error) {
-    localConfig = {};
-  }
-}
-
-const apiBaseUrl = localConfig.apiBaseUrl || "https://jialutong.cloud";
+const apiBaseUrl = "https://jialutong.cloud";
 
 module.exports = {
   apiBaseUrl,
-  uploadUrl: localConfig.uploadUrl || `${apiBaseUrl}/api/files`
+  uploadUrl: `${apiBaseUrl}/api/files`
 };
